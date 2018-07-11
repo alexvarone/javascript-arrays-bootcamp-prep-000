9526fcd972a5eceac7cfd0102b3246819a425243
Alex Varone <alexvarone@ymail.com>
2018-07-11 16:49:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var candyStr = "Starburst";
 
 function addElementToBeginningOfArray(chocolateBars, candyStr){
-  return chocolateBars.unshift()
+  return [chocolateBars...candyStr];
 }
 
 
